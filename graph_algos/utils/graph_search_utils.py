e71f3f8b6f7de8b7fa0220df9b74c487f25239d3
Mauricio Bedoya <bedoyama@gmail.com>
2025-09-16 15:16:45 -0500

    dfs with component labeling

--- graph_algos/utils/graph_search_utils.py
+++ graph_algos/utils/graph_search_utils.py
@@ -44,3 +44,47 @@ class GraphSearchUtils:
             if not seen[ind]:
                 GraphSearchUtils.dfs_recursive_path(g, ind, seen, last)
         return last
+
+    @staticmethod
+    def depth_first_search_stack(g: Graph, start: int) -> list: 
+        seen: list = [False] * g.num_nodes
+        last: list = [-1] * g.num_nodes
+        to_explore: list = []
+
+        to_explore.append(start)
+        while to_explore:
+            ind = to_explore.pop()
+            if not seen[ind]:
+                current: Node = g.nodes[ind]
+                seen[ind] = True
+
+            all_edges: list = current.get_sorted_edge_list()
+            all_edges.reverse()
+            for edge in all_edges:
+                neighbor: int = edge.to_node
+                if not seen[neighbor]:
+                    last[neighbor] = ind
+                    to_explore.append(neighbor)
+        return last
+
+    @staticmethod
+    def dfs_recursive_cc(g: Graph, ind: int, component: list, curr_comp: int):  
+        component[ind] = curr_comp
+        current: Node = g.nodes[ind]
+
+        for edge in current.get_edge_list():
+            neighbor: int = edge.to_node
+            if component[neighbor] == -1:
+                GraphSearchUtils.dfs_recursive_cc(g, neighbor, component, curr_comp)
+
+    @staticmethod
+    def dfs_connected_components(g: Graph) -> list:
+        component: list = [-1] * g.num_nodes
+        curr_comp: int = 0
+
+        for ind in range(g.num_nodes):
+            if component[ind] == -1:
+                GraphSearchUtils.dfs_recursive_cc(g, ind, component, curr_comp)
+                curr_comp += 1
+
+        return component
