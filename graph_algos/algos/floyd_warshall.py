cae16536bbf675516d88384690b238ab0d6cf846
Mauricio Bedoya <bedoyama@gmail.com>
2025-09-22 16:24:49 -0500

    Floyd Warshall

---
+++ graph_algos/algos/floyd_warshall.py
@@ -0,0 +1,29 @@
+import math
+from typing import Union
+from graph_algos.entities.graph.edge import Edge
+from graph_algos.entities.graph.graph import Graph
+
+
+class FloydWarshall:
+
+    def compute(g: Graph) -> list: 
+        N: int = g.num_nodes
+        cost: list = [[math.inf] * N for _ in range(N)]
+        last: list = [[-1] * N for _ in range(N)]
+        for i in range(N):
+            for j in range(N):
+                if i == j:
+                    cost[i][j] = 0.0
+                else:
+                    edge: Union[Edge, None] = g.get_edge(i, j)
+                    if edge is not None:
+                        cost[i][j] = edge.weight
+                        last[i][j] = i
+
+        for k in range(N):
+            for i in range(N):
+                for j in range(N):
+                    if cost[i][j] > cost[i][k] + cost[k][j]:
+                        cost[i][j] = cost[i][k] + cost[k][j]
+                        last[i][j] = last[k][j]
+        return last
---
+++ graph_algos/utils/graph_diameter.py
@@ -0,0 +1,32 @@
+import math
+from typing import Union
+from graph_algos.algos.floyd_warshall import FloydWarshall
+from graph_algos.entities.graph.edge import Edge
+from graph_algos.entities.graph.graph import Graph
+
+
+class GraphDiameter:
+    @staticmethod
+    def compute(g: Graph) -> float: 
+        fwComputer = FloydWarshall()
+        last: list = fwComputer.compute(g)
+        max_cost: float = -math.inf
+
+        for i in range(g.num_nodes):
+            for j in range(g.num_nodes):
+                cost: float = 0.0
+                current: int = j
+
+                while current != i:
+                    prev: int = last[i][current]
+                    if prev == -1:
+                        return math.inf
+
+                    edge: Union[Edge, None] = g.get_edge(prev, current)
+                    cost = cost + edge.weight
+                    current = prev
+
+                if cost > max_cost:
+                    max_cost = cost
+
+        return max_cost
